44cd47c0c7b7fb4cfcf10a0d8f3df382eed11492
Beth Wickerson <beth.wickerson@signaltheory.com>
2022-12-18 22:34:50 -0600

    url to public sketch

--- src/components/Sketch.js
+++ src/components/Sketch.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Sketch = () => {
   return (
-    <iframe className="sketch" title="animated sketch" src=""></iframe>
+    <iframe className="sketch" title="animated sketch" src="https://blrajreactsitemain.gatsbyjs.io/p5sketch/index.html"></iframe>
   )
 }
 
