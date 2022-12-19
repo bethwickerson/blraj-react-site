15982fca23dd750241227b3def595d5d20bbbd09
Beth Wickerson <beth.wickerson@signaltheory.com>
2022-12-19 12:06:25 -0600

    fix iframe overlay scrolling

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,22 +5,24 @@ import { yourData } from "../yourdata";
 import Sketch from "../components/iFrameSketch"
 
 const Header = ({ siteTitle }) => (
-  <header className="section header" id="home">
+  <>
     <Sketch />
-    <div className="container">
-      <div className="header-wrapper">
-        <Fade>
-          <h1>
-            {yourData.headername}
-          </h1>
-        </Fade>
-        <Fade delay={500}>
-          <button className="primary-btn" onClick={() => scrollTo("#contact")}>Work with me (◔◡◔)</button>
-        </Fade>
-        {/* <button className="arrow-btn bounce" onClick={() => scrollTo("#work")}>&#8595;</button> */}
+    <header className="section header" id="home">
+      <div className="container">
+        <div className="header-wrapper">
+          <Fade>
+            <h1>
+              {yourData.headername}
+            </h1>
+          </Fade>
+          <Fade delay={500}>
+            <button className="primary-btn" onClick={() => scrollTo("#contact")}>Work with me (◔◡◔)</button>
+          </Fade>
+          {/* <button className="arrow-btn bounce" onClick={() => scrollTo("#work")}>&#8595;</button> */}
+        </div>
       </div>
-    </div>
-  </header>
+    </header>
+  </>
 )
 
 export default Header
