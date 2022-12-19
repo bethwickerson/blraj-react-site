552393a9157a0dc6b47b2aa17e757e2379ca0e72
Beth Wickerson <beth.wickerson@signaltheory.com>
2022-12-18 21:55:13 -0600

    p5 iframe

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,7 @@ import * as React from "react";
 import Layout from "../components/layout";
 import Seo from "../components/seo";
 // Components
+import Sketch from '../components/Sketch';
 import Navbar from "../components/Navbar";
 import Work from "../components/Work";
 import About from "../components/About";
@@ -11,6 +12,7 @@ import Contact from "../components/Contact";
 
 const IndexPage = () => (
   <Layout>
+    <Sketch />
     <Navbar />
     <Work />
     <Promotion />
